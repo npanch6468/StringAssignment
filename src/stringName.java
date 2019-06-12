9bfdb6138fb078cdef56174ace97d31277b5d8bc
npanch6468 <nagapanchumarty@gmail.com>
2019-06-12 09:59:20 -0400

    first commit

---
+++ src/stringName.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class stringName {
+    public static void main(String [] args){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Print your name here: ");
+        String name = myScanner.next();
+        System.out.println("Your name is : " + name);
+
+    }
+}
